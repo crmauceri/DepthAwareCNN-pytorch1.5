df0d3c0695bb2822815e8759118b5b71629d4296
Cecilia <cema3961@colorado.edu>
2020-04-13 16:19:31 -0700

    Indentation error

--- models/Deeplab.py
+++ models/Deeplab.py
@@ -172,15 +172,15 @@ class Deeplab_Solver(BaseModel):
 
         self.writer.add_scalar(self.opt.name+'/Learning_Rate/', lr, step)
 
-	self.optimizer.param_groups[0]['lr'] = lr
-	self.optimizer.param_groups[1]['lr'] = lr
-	self.optimizer.param_groups[2]['lr'] = lr
-	self.optimizer.param_groups[3]['lr'] = lr
-	# self.optimizer.param_groups[0]['lr'] = lr
-	# self.optimizer.param_groups[1]['lr'] = lr*10
-	# self.optimizer.param_groups[2]['lr'] = lr*2 #* 100
-	# self.optimizer.param_groups[3]['lr'] = lr*20
-	# self.optimizer.param_groups[4]['lr'] = lr*100
+        self.optimizer.param_groups[0]['lr'] = lr
+        self.optimizer.param_groups[1]['lr'] = lr
+        self.optimizer.param_groups[2]['lr'] = lr
+        self.optimizer.param_groups[3]['lr'] = lr
+        # self.optimizer.param_groups[0]['lr'] = lr
+        # self.optimizer.param_groups[1]['lr'] = lr*10
+        # self.optimizer.param_groups[2]['lr'] = lr*2 #* 100
+        # self.optimizer.param_groups[3]['lr'] = lr*20
+        # self.optimizer.param_groups[4]['lr'] = lr*100
 
 
         # torch.nn.utils.clip_grad_norm(self.model.Scale.get_1x_lr_params_NOscale(), 1.)
