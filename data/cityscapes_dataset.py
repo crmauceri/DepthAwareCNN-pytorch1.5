592cc96d6cdc4668830b0ff53a35f049181c0a93
Cecilia <cema3961@colorado.edu>
2020-04-13 16:13:15 -0700

    Add cityscapes dataloader

--- data/cityscapes_dataset.py
+++ data/cityscapes_dataset.py
@@ -8,53 +8,55 @@ from data.base_dataset import *
 from PIL import Image
 import math, random
 import time
+from pathlib import Path
 
-def make_dataset_fromlst(dataroot, listfilename):
+#label_weight = [0.005770029194127712, 0.012971614093310078, 0.03362765598112945, 0.1221253676849356, 0.06859890961300749, 0.15823995906267385, 0.09602253559800432, 0.12810205801896177, 0.1718342979655409, 0.2830090542974214, 0.06808788822945917, 0.28288925581409397, 0.30927228790865696, 0.6046432911319981, 0.7276073719428268, 0.6584037740058684, 1.6161287361233052, 0.4147706187681264, 0.8706942889933341, 0.8146644289372541, 0.8744887302745185, 0.25134887482271207, 0.3527236656093415, 1.9965490899244573, 3.453731279765878, 0.603116521402235, 1.6573996378194742, 21.603576890926714, 1.3738455233450662, 11.13489209800063, 7.110616094064334, 3.5123361407056404, 8.061760999036036, 1.5451820155073996, 0.9412019674579293, 9.351917523626016, 0.8485119225668366, 0.09619406694759904, 0.07387533823120886, 0.019189673545819297]
+
+def make_dataset_frombasedir(basedir):
     """
-    NYUlist format:
-    imagepath seglabelpath depthpath HHApath
+    Cityscapes format:
+    All data in similar directory structure with common base directory
     """
-    images = []
+
+    # 'troisdorf_000000_000073' is corrupted
+    images = [x for x in recursive_glob(rootdir=basedir, suffix='.png') if 'troisdorf_000000_000073' not in x]
     segs = []
     depths = []
     HHAs = []
-    with open(listfilename) as f:
-        content = f.readlines()
-        for x in content:
-            imgname, segname, depthname, HHAname = x.strip().split(' ')
-            images += [os.path.join(dataroot, imgname)]
-            segs += [os.path.join(dataroot, segname)]
-            depths += [os.path.join(dataroot, depthname)]
-            HHAs += [os.path.join(dataroot, HHAname)]
+    for x in images:
+        img_dir, img_path = os.path.split(x)
+        segs.append(os.path.join(img_dir.replace('leftImg8bit', 'gtCoarse'), img_path.replace('leftImg8bit', 'labelIds')))
+        depths.append(x.replace('leftImg8bit', 'disparity'))
+        HHAs.append(x.replace('leftImg8bit', 'HHA'))
+
     return {'images':images, 'segs':segs, 'HHAs':HHAs, 'depths':depths}
 
-class SUNRGBDDataset(BaseDataset):
+def recursive_glob(rootdir='.', suffix=''):
+    """Performs recursive glob with given suffix and rootdir
+        :param rootdir is the root directory
+        :param suffix is the suffix to be searched
+    """
+    return [os.path.join(looproot, filename)
+            for looproot, _, filenames in os.walk(rootdir)
+            for filename in filenames if filename.endswith(suffix)]
+
+class CityscapesDataset(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
         np.random.seed(int(time.time()))
-        self.paths_dict = make_dataset_fromlst(opt.dataroot, opt.list)
+        self.paths_dict = make_dataset_frombasedir(opt.list)
         self.len = len(self.paths_dict['images'])
-        # self.label_weight = torch.Tensor(label_weight)
-        self.datafile = 'sunrgbd_dataset.py'
 
     def __getitem__(self, index):
         #self.paths['images'][index]
         # print self.opt.scale,self.opt.flip,self.opt.crop,self.opt.colorjitter
         img = np.asarray(Image.open(self.paths_dict['images'][index]))#.astype(np.uint8)
-        HHA = np.asarray(Image.open(self.paths_dict['HHAs'][index]))[:,:,::-1]
-        seg = np.asarray(Image.open(self.paths_dict['segs'][index])).astype(np.uint8)-1
-        depth = np.asarray(Image.open(self.paths_dict['depths'][index])).astype(np.uint16)
+        depth = np.asarray(Image.open(self.paths_dict['depths'][index])).astype(np.float32)/120. # 1/10 * depth
 
-        assert (img.shape[0]==HHA.shape[0]==seg.shape[0]==depth.shape[0])
-        assert (img.shape[1]==HHA.shape[1]==seg.shape[1]==depth.shape[1])
+        HHA = np.asarray(Image.open(self.paths_dict['HHAs'][index]))
+        seg = np.asarray(Image.open(self.paths_dict['segs'][index])).astype(np.uint8)
 
-        depth = np.bitwise_or(np.right_shift(depth,3),np.left_shift(depth,16-3))
-        depth = depth.astype(np.float32)/120. # 1/5 * depth
-
-
-
-
-        params = get_params_sunrgbd(self.opt, seg.shape, maxcrop=.7)
+        params = get_params(self.opt, seg.shape)
         depth_tensor_tranformed = transform(depth, params, normalize=False,istrain=self.opt.isTrain)
         seg_tensor_tranformed = transform(seg, params, normalize=False,method='nearest',istrain=self.opt.isTrain)
         if self.opt.inputmode == 'bgr-mean':
@@ -64,10 +66,6 @@ class SUNRGBDDataset(BaseDataset):
             img_tensor_tranformed = transform(img, params, istrain=self.opt.isTrain, option=1)
             HHA_tensor_tranformed = transform(HHA, params, istrain=self.opt.isTrain, option=2)
 
-
-        # print img_tensor_tranformed
-        # print(np.unique(depth_tensor_tranformed.numpy()).shape)
-        # print img_tensor_tranformed.size()
         return {'image':img_tensor_tranformed,
                 'depth':depth_tensor_tranformed,
                 'seg': seg_tensor_tranformed,
@@ -78,28 +76,24 @@ class SUNRGBDDataset(BaseDataset):
         return self.len
 
     def name(self):
-        return 'sunrgbd_dataset'
+        return 'CityscapesDataset'
 
-class SUNRGBDDataset_val(BaseDataset):
+class CityscapesDataset_val(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
         np.random.seed(8964)
-        self.paths_dict = make_dataset_fromlst(opt.vallist)
+        self.paths_dict = make_dataset_frombasedir(opt.vallist)
         self.len = len(self.paths_dict['images'])
 
     def __getitem__(self, index):
         #self.paths['images'][index]
         img = np.asarray(Image.open(self.paths_dict['images'][index]))#.astype(np.uint8)
-        HHA = np.asarray(Image.open(self.paths_dict['HHAs'][index]))[:,:,::-1]
-        seg = np.asarray(Image.open(self.paths_dict['segs'][index])).astype(np.uint8)-1
-        depth = np.asarray(Image.open(self.paths_dict['depths'][index])).astype(np.uint16)
-        depth = np.bitwise_or(np.right_shift(depth,3),np.left_shift(depth,16-3))
-        depth = depth.astype(np.float32)/120. # 1/5 * depth
+        # print (img)
+        depth = np.asarray(Image.open(self.paths_dict['depths'][index])).astype(np.float32)/120. # 1/5 * depth
+        HHA = np.asarray(Image.open(self.paths_dict['HHAs'][index]))
+        seg = np.asarray(Image.open(self.paths_dict['segs'][index])).astype(np.uint8)
 
-        assert (img.shape[0]==HHA.shape[0]==seg.shape[0]==depth.shape[0])
-        assert (img.shape[1]==HHA.shape[1]==seg.shape[1]==depth.shape[1])
-
-        params = get_params_sunrgbd(self.opt, seg.shape, test=True)
+        params = get_params(self.opt, seg.shape, test=True)
         depth_tensor_tranformed = transform(depth, params, normalize=False,istrain=self.opt.isTrain)
         seg_tensor_tranformed = transform(seg, params, normalize=False,method='nearest',istrain=self.opt.isTrain)
         # HHA_tensor_tranformed = transform(HHA, params,istrain=self.opt.isTrain)
@@ -120,6 +114,8 @@ class SUNRGBDDataset_val(BaseDataset):
         return self.len
 
     def name(self):
-        return 'sunrgbd_dataset_Val'
+        return 'CityscapesDataset'
 
 
+if __name__ == '__main__':
+    make_dataset_frombasedir('datasets/cityscapes/leftImg8bit/')
