0b0eaf573ce628c3dc0369dfa230e5ae01750638
Cecilia <cema3961@colorado.edu>
2020-05-27 15:07:52 -0700

    Debugging statements

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -345,7 +345,7 @@ std::vector<torch::Tensor> depthconv_backward_cuda(
         std::cout << string_format("gradOutput_n_slice dim: %i", gradOutput_n_slice.ndimension()) << std::endl;
         std::cout << string_format("gradOutput_n_slice: %i x %i", gradOutput_n_slice.size(0), gradOutput_n_slice.size(1)) << std::endl;
         std::cout << string_format("ones dim: %i", ones.ndimension()) << std::endl;
-        std::cout << string_format("ones: %i x %i", ones.size(0), ones.size(1)) << std::endl;
+        std::cout << string_format("ones: %i x %i", ones.size(0)) << std::endl;
 
         std::cout << string_format("gradWeight dim: %i", gradWeight.ndimension()) << std::endl;
         std::cout << string_format("gradWeight: %i x %i", gradWeight.size(0), gradWeight.size(1)) << std::endl;
