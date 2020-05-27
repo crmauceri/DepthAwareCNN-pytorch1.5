55d0fc27a2385b8cf34a00db3f7057ed30b93f7e
Cecilia <cema3961@colorado.edu>
2020-05-27 16:32:05 -0700

    Debugging statements

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -354,10 +354,8 @@ std::vector<torch::Tensor> depthconv_backward_cuda(
 
         std::cout << string_format("gradOutput_n_slice dim: %i", gradOutput_n_slice.ndimension()) << std::endl;
         std::cout << string_format("gradOutput_n_slice: %i x %i", gradOutput_n_slice.size(0), gradOutput_n_slice.size(1)) << std::endl;
-        std::cout << string_format("gradWeight_slice dim: %i", gradWeight_slice.ndimension()) << std::endl;
-        std::cout << string_format("gradWeight_slice: %i x %i", gradWeight_slice.size(0), gradWeight_slice.size(1)) << std::endl;
 
-        gradWeight_slice.addmm_(gradOutput_n_slice.transpose(1,0), columns, /*beta=*/1.0, /*alpha=*/scale);
+        gradWeight.addmm_(gradOutput_n_slice.transpose(1,0), columns, /*beta=*/1.0, /*alpha=*/scale);
 
         // Do Bias:
 //       std::cout << string_format("gradOutput_n_slice dim: %i", gradOutput_n_slice.ndimension()) << std::endl;
@@ -365,7 +363,7 @@ std::vector<torch::Tensor> depthconv_backward_cuda(
         std::cout << string_format("ones dim: %i", ones.ndimension()) << std::endl;
         std::cout << string_format("ones: %i x %i", ones.size(0), ones.size(1)) << std::endl;
         std::cout << string_format("gradBias dim: %i", gradBias.ndimension()) << std::endl;
-        std::cout << string_format("gradBias: %i x %i", gradBias.size(0), gradBias.size(1)) << std::endl;
+        std::cout << string_format("gradBias: %i", gradBias.size(0)) << std::endl;
 
         gradBias.addmm_(ones, gradOutput_n_slice, /*beta=*/1.0, /*alpha=*/scale);
     }
