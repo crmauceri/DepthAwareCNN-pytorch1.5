69ac7911f98f30ec7dd27defcdee5b8d301143f1
Cecilia <cema3961@colorado.edu>
2020-05-28 11:11:52 -0700

    Debugging

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -339,11 +339,12 @@ std::vector<torch::Tensor> depthconv_backward_cuda(
 
     //Compute weight gradient
     torch::Tensor gradWeight_flat = gradWeight.view({nOutputPlane, weight.size(1)*weight.size(2)*weight.size(3)});
-    torch::Tensor product = torch::matmul(gradOutput.view({gradOutput.size(0),gradOutput.size(1), gradOutput.size(2)*gradOutput.size(3)}).transpose(2, 1), gradWeight_flat);
+    torch::Tensor product = torch::matmul(columns.tranpose(2,1),
+                                          gradOutput.view({gradOutput.size(0), gradOutput.size(1),
+                                                           gradOutput.size(2)*gradOutput.size(3)}).transpose(2,1));
 
     std::cout << string_format("product dim: %i", product.ndimension()) << std::endl;
     std::cout << string_format("product: %i x %i x %i", product.size(0), product.size(1), product.size(2)) << std::endl;
-
 
     gradWeight_flat.add_(product);
 
