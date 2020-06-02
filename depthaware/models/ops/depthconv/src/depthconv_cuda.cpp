f4ae69f4990b825282b5a732baba4e5f654160f6
Cecilia <cema3961@colorado.edu>
2020-06-02 14:05:12 -0700

    Fix typo

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -321,7 +321,7 @@ torch::Tensor depthconv_weight_grad(torch::Tensor input, torch::Tensor input_dep
     torch::Tensor gradWeight = torch::zeros({nOutputPlane, nInputPlane, kW, kH});
 
     for(int elt=0; elt<batchSize; elt++){
-        torch::Tensor gradOutput_n = gradOutput_padded.select(0, elt);
+        torch::Tensor gradOutput_n = gradOutput.select(0, elt);
         torch::Tensor depth_n = input_depth.select(0, elt);
         torch::Tensor input_n = input.select(0, elt);
 
