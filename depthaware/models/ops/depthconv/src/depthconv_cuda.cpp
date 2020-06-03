2910c97b5b3e418f51b77d182d45d05f644aa63e
Cecilia <cema3961@colorado.edu>
2020-06-02 18:13:19 -0700

    Fix dimensions

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -341,8 +341,8 @@ torch::Tensor depthconv_weight_grad(torch::Tensor input, torch::Tensor input_dep
         std::cout << gradOutput_n << std::endl;
 
         //Multiplication with reshaped input is equivalent to 2d convolution
-        torch::Tensor product = torch::matmul(gradOutput_n, columns);
-        gradWeight.add_(product);
+        torch::Tensor product = torch::matmul(gradOutput_n, columns.transpose(2,3));
+        gradWeight.add_(product.reshape({nOutputPlane, nInputPlane, kW,  kH}));
     }
 
     std::cout << string_format("gradWeight dim: %i", gradWeight.ndimension()) << std::endl;
