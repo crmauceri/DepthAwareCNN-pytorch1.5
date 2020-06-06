b56792bdb59abf1011d09af88a5443944f9c6c8b
Cecilia <cema3961@colorado.edu>
2020-06-05 20:05:05 -0700

    More tests

--- depthaware/models/ops/depthconv/tests.py
+++ depthaware/models/ops/depthconv/tests.py
@@ -1,3 +1,7 @@
+import torch
+import numpy as np
+import depthconv
+
 def output_size(input, weight, padding, dilation, stride):
     channels = weight.size(0)
 
@@ -14,10 +18,17 @@ def output_size(input, weight, padding, dilation, stride):
                 'x'.join(map(str, output_size))))
     return output_size
 
+class TestLoss(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, target):
+        return input - target
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output, None
+
+
 if __name__ == '__main__':
-    import torch
-    import numpy as np
-    import depthconv
 
     #TODO check dilation and stride
     batch_size = 1
@@ -39,6 +50,7 @@ if __name__ == '__main__':
     outsize = output_size(input, weight, padding, dilation, stride)
     grad_output = torch.FloatTensor(range(outsize[0]*outsize[1]*outsize[2]*outsize[3])).cuda().reshape(outsize)
 
+    print("Toy grad output:")
     print(grad_output)
 
     grad_input, grad_weight, grad_bias = depthconv.backward(
@@ -46,4 +58,24 @@ if __name__ == '__main__':
         weight.size(3), weight.size(2), stride[1], stride[0],
         padding[1], padding[0], dilation[1], dilation[0], 1.0)
 
+    print("DepthConv input gradient:")
     print(grad_input)
+
+    conv_layer = torch.nn.Conv2d(out_channels, kernel_size, kernel_size, bias=True, stride=stride, padding=padding,
+                                 dilation=dilation, groups=1)
+    conv_layer.weight = torch.nn.Parameter(weight, requires_grad=True)
+    bias = torch.zeros((out_channels), device=device)
+    conv_layer.bias = torch.nn.Parameter(bias.squeeze(1), requires_grad=True)
+
+    input = torch.ones((batch_size, 3, w, h), device=device, requires_grad=True)
+    x = conv_layer(input)
+    target = torch.zeros(x.shape, device=device)
+    loss = TestLoss.apply(x, target)
+    loss.backward(grad_output)
+
+    print("Pytorch input gradient:")
+    print(input.grad)
+
+    np.testing.assert_array_almost_equal(grad_input, input.grad)
+    np.testing.assert_array_almost_equal(grad_weight, conv_layer.weight.grad)
+    np.testing.assert_array_almost_equal(grad_bias, conv_layer.bias.grad)
