d05d8a7f0d11b835c7f6b0a6d54d81bbd856cec1
Cecilia <cema3961@colorado.edu>
2020-06-17 14:19:28 -0700

    Try assert true instead of assert equal

--- depthconv_unit_tests.py
+++ depthconv_unit_tests.py
@@ -94,7 +94,7 @@ class DepthConvTests(unittest.TestCase):
                            target, grad_output)
 
         for pair in result:
-            self.assertEqual(pair['tensors'][0], pair['tensors'][1],
+            self.assertTrue(all(pair['tensors'][0] == pair['tensors'][1]),
                         msg="Variable {} is not equal within 5 sig figs".format(pair['var_name']))
 
 
