3c7434faa568323788b17b0c09e46bb8ca0eaed8
Cecilia <cema3961@colorado.edu>
2020-06-29 08:50:45 -0700

    Switched width and height

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -379,7 +379,7 @@ torch::Tensor depthconv_weight_grad(torch::Tensor input, torch::Tensor input_dep
             //In backward pass of convolution, stride and dilation switch roles
             torch::Tensor columns = depthconv_im2col(input_n_c, depth_n, alpha,
                     1, gradW, gradH,
-                    gH, gW,
+                    gW, gH,
                     padH, padW,
                     dilationH, dilationW,
                     strideH, strideW);
