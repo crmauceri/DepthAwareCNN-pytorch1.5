66999342955624a1b6fb3efbf783b80d05521938
Cecilia <cema3961@colorado.edu>
2020-06-29 17:04:25 -0700

    Fix padding dimension

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -380,7 +380,7 @@ torch::Tensor depthconv_weight_grad(torch::Tensor input, torch::Tensor input_dep
             torch::Tensor columns = depthconv_im2col(input_n_c, depth_n, alpha,
                     1, gradW, gradH,
                     gW, gH,
-                    padH, padW,
+                    0, 0,
                     dilationH, dilationW,
                     strideH, strideW);
 
