a0baceca9251fc4846c20bedf2861a6d1112cea4
Cecilia <cema3961@colorado.edu>
2020-07-09 15:39:17 -0700

    Debugging

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -374,11 +374,13 @@ torch::Tensor depthconv_weight_grad(torch::Tensor input, torch::Tensor input_dep
         //Reshape input and gradOutput with depth difference
         //In backward pass of convolution, stride and dilation switch roles
         torch::Tensor columns = depthconv_im2col(input_n, depth_n, alpha,
-                nInputPlane, gradW, gradH,
+                1, gradW, gradH,
                 gW, gH,
                 padH, padW,
                 dilationH, dilationW,
                 strideH, strideW);
+
+        columns = columns.repeat({1, nInputPlane});
 
         std::cout << string_format("columns: %i x %i ", columns.size(0), columns.size(1)) +
                  string_format("gradOutput: %i x %i ", gradOutput_n.size(0), gradOutput_n.size(1)) << std::endl;
