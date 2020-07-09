9169732aeafc8782b1ce1e3f991d2ec69aed5e5d
Cecilia <cema3961@colorado.edu>
2020-07-09 14:32:39 -0700

    Try to fix dimentions

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -381,7 +381,7 @@ torch::Tensor depthconv_weight_grad(torch::Tensor input, torch::Tensor input_dep
             //Reshape input and gradOutput with depth difference
             //In backward pass of convolution, stride and dilation switch roles
             torch::Tensor columns = depthconv_im2col(input_n, depth_n, alpha,
-                    nInputPlane, gradW, gradH,
+                    1, gradW, gradH,
                     gW, gH,
                     padH, padW,
                     dilationH, dilationW,
