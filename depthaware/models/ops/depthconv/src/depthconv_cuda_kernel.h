f1ec1e89460bca99580deb7eabff9476e6d70da0
Cecilia <cema3961@colorado.edu>
2020-07-10 15:56:04 -0700

    Try different dimentions

--- depthaware/models/ops/depthconv/src/depthconv_cuda_kernel.h
+++ depthaware/models/ops/depthconv/src/depthconv_cuda_kernel.h
@@ -13,5 +13,15 @@ torch::Tensor depthconv_im2col(
     const int stride_h, const int stride_w,
     const int dilation_h, const int dilation_w);
 
+torch::Tensor depthconv_gradOut2col(
+    torch::Tensor data_im,
+    torch::Tensor data_depth,
+    const double alpha,
+    const int channels, const int height, const int width,
+    const int ksize_h, const int ksize_w,
+    const int pad_h, const int pad_w,
+    const int stride_h, const int stride_w,
+    const int dilation_h, const int dilation_w);
+
 #endif
 
