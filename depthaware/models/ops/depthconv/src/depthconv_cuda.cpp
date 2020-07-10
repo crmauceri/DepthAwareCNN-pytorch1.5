3cb260df2e5724ef59676670e61d037b2e82a93a
Cecilia <cema3961@colorado.edu>
2020-07-10 11:06:01 -0700

    Try different dimentions

--- depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
+++ depthaware/models/ops/depthconv/src/depthconv_cuda.cpp
@@ -386,13 +386,14 @@ torch::Tensor depthconv_weight_grad(torch::Tensor input, torch::Tensor input_dep
         columns = columns.repeat({nInputPlane, 1}).permute({1, 0});
         std::cout << columns << std::endl;
 
-        torch::Tensor offset = torch::zeros({nInputPlane, 1}, torch::kCUDA);
+        torch::Tensor offset = torch::zeros({columns.size(0), columns.size(1)}, torch::kCUDA);
 
         for(int j=1; j<nInputPlane; j++){
-            offset.index_put_({j, 0}, gradW*gradH*j);
+            using namespace torch::indexing;
+            int val = gradW*gradH*j;
+            offset.index_put_({Ellipsis, Slice(columns.size(1)/nInputPlane*j, columns.size(1)/nInputPlane*(j+1))}, val);
         }
 
-        offset = offset.repeat({columns.size(1)/nInputPlane, columns.size(0)}).permute({1, 0}).reshape({columns.size(0), columns.size(1)});
         columns = columns + offset;
         std::cout << offset << std::endl;
         std::cout << columns << std::endl;
